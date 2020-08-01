84df91343efdd77474ae92627a98373975a4b5cf
Guilherme Sanches <guilherme@pop-os.localdomain>
2020-08-01 18:47:33 -0300

    Load image on init

--- components/ProfileUserImage.js
+++ components/ProfileUserImage.js
@@ -1,148 +1,162 @@
-import * as React from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-} from 'react-native';
-import gatao from '../assets/images/gatao.jpeg'
-import { Avatar } from 'react-native-elements'
-import * as ImagePicker from 'expo-image-picker'
-import Constants from 'expo-constants';
-import { ProfileRoundImg } from '../components/CustomElements'
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import gatao from "../assets/images/DefaultUserAlt.jpg";
+import { Avatar } from "react-native-elements";
+import * as ImagePicker from "expo-image-picker";
+import Constants from "expo-constants";
+import { ProfileRoundImg } from "../components/CustomElements";
 
+const ProfileInfo = (props) => {
+  return (
+    <View style={styles.userInfo}>
+      <Text>Marcelo Reis</Text>
+      <Text>@marretinha.mil_gr$au</Text>
+    </View>
+  );
+};
 
-const ProfileInfo = props => {
+export default ProfileImage = (props) => {
+  const { firebase } = props;
+  const [image, setImage] = React.useState(null);
+  const [_state, setState] = React.useState({});
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const userImage = await firebase.FIREBASE.storage()
+          .ref(`ProfileImg/${firebase.token}.jpg`)
+          .getDownloadURL();
+        setImage(userImage);
+      } catch (e) {
+          console.log('nao deua')
+      }
 
-    return (
-        <View style={styles.userInfo}>
-            <Text>Marcelo Rei</Text>
-            <Text>@marretinha.mil_gr$au</Text>
-        </View>
-    )
-}
-
-export default ProfileImage = props => {
-    const { firebase } = props
-    const [image, setImage] = React.useState(null);
-    const [_state, setState] = React.useState({})
-    React.useEffect(() => {
-        (async () => {
-            if (Constants.platform.ios) {
-                const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
-                if (status !== 'granted') {
-                    alert('Sorry, we need camera roll permissions to make this work!');
-                }
-            }
-        })();
-    }, []);
-
-    React.useEffect(() => {
-        if (_state.progress === 100) {
-            setTimeout(() => {
-                setState({
-                    ..._state,
-                    progress: 0
-                })
-            }, 2000)
+      if (Constants.platform.ios) {
+        const {
+          status,
+        } = await ImagePicker.requestCameraRollPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
         }
-    }, [_state])
+      }
+    })();
+  }, []);
 
-    const pickImage = async () => {
-        try {
-            let result = await ImagePicker.launchImageLibraryAsync({
-                mediaTypes: ImagePicker.MediaTypeOptions.All,
-                allowsEditing: true,
-                aspect: [4, 4],
-                quality: 1,
-            });
+  React.useEffect(() => {
+    if (_state.progress === 100) {
+      setTimeout(() => {
+        setState({
+          ..._state,
+          progress: 0,
+        });
+      }, 2000);
+    }
+  }, [_state]);
 
-            const ext = result.uri.split('.').pop(); // Extract image extension
-            const response = await fetch(result.uri);
-            const blob = await response.blob();
-            firebase.FIREBASE.storage().ref().child(`ProfileImg/${firebase.token}.${ext}`).put(blob)
-                .on(
-                    firebase.FIREBASE.storage.TaskEvent.STATE_CHANGED,
-                    snapshot => {
-                        let state = {};
-                        state = {
-                            ...state,
-                            progress: (snapshot.bytesTransferred / snapshot.totalBytes) * 100 // Calculate progress percentage
-                        };
-                        if (snapshot.state === firebase.FIREBASE.storage.TaskState.SUCCESS) {
-                            const allImages = _state.images;
-                            allImages.push(snapshot.downloadURL);
-                            state = {
-                                ...state,
-                                uploading: false,
-                                imgSource: '',
-                                imageUri: '',
-                                progress: 0,
-                                images: allImages
-                            };
-                            // AsyncStorage.setItem('images', JSON.stringify(allImages));
-                        }
-                        setState(state);
-                    },
-                    error => {
-                        unsubscribe();
-                        alert('Sorry, Try again.');
-                    }
-                );
+  const pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 4],
+        quality: 1,
+      });
 
-            if (!result.cancelled) {
-                setImage(result.uri);
+      const ext = result.uri.split(".").pop(); // Extract image extension
+      const response = await fetch(result.uri);
+      const blob = await response.blob();
+      firebase.FIREBASE.storage()
+        .ref()
+        .child(`ProfileImg/${firebase.token}.jpg`)
+        .put(blob)
+        .on(
+          firebase.FIREBASE.storage.TaskEvent.STATE_CHANGED,
+          (snapshot) => {
+            let state = {};
+            state = {
+              ...state,
+              progress: (snapshot.bytesTransferred / snapshot.totalBytes) * 100, // Calculate progress percentage
+            };
+            if (
+              snapshot.state === firebase.FIREBASE.storage.TaskState.SUCCESS
+            ) {
+              const allImages = _state.images;
+              allImages.push(snapshot.downloadURL);
+              state = {
+                ...state,
+                uploading: false,
+                imgSource: "",
+                imageUri: "",
+                progress: 0,
+                images: allImages,
+              };
+              // AsyncStorage.setItem('images', JSON.stringify(allImages));
             }
-        } catch (e) {
-            console.log(e)
-        }
-    };
+            setState(state);
+          },
+          (error) => {
+            unsubscribe();
+            alert("Sorry, Try again.");
+          }
+        );
 
-    return (
-        <View style={{ justifyContent: 'center' }}>
-            <View style={styles.ProfilePhotoContainer}>
-                <View>
-                    <Avatar
-                        size={110}
-                        rounded
-                        source={image ? { uri: image } : gatao}
-                        icon={{ type: 'Feather', name: 'camera', size: 24, color: 'black' }}
-                        accessory={{ type: 'feather', name: 'camera', size: 24, color: '#9597A1', style: styles.Icon }}
-                        showAccessory={true}
-                        ImageComponent={ProfileRoundImg}
-                        onPress={_ => console.log('clicked')}
-                        onAccessoryPress={pickImage}
-                    />
-                </View>
-                <ProfileInfo />
+      if (!result.cancelled) {
+        setImage(result.uri);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-            </View>
-            <View
-                style={[styles.progressBar, { width: `${_state.progress || 0}%` }]}
-            />
+  return (
+    <View style={{ justifyContent: "center" }}>
+      <View style={styles.ProfilePhotoContainer}>
+        <View>
+          <Avatar
+            size={110}
+            rounded
+            source={image ? { uri: image } : gatao}
+            icon={{ type: "Feather", name: "camera", size: 24, color: "black" }}
+            accessory={{
+              type: "feather",
+              name: "camera",
+              size: 24,
+              color: "#9597A1",
+              style: styles.Icon,
+            }}
+            showAccessory={true}
+            ImageComponent={ProfileRoundImg}
+            onPress={(_) => console.log("clicked")}
+            onAccessoryPress={pickImage}
+          />
         </View>
-
-    )
-}
+        <ProfileInfo />
+      </View>
+      <View
+        style={[styles.progressBar, { width: `${_state.progress || 0}%` }]}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    ProfilePhotoContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    Icon: {
-        backgroundColor: '#e4e7ff',
-        borderRadius: 100,
-        width: 35,
-        height: 34,
-        justifyContent: 'center'
-    },
-    userInfo: {
-        marginLeft: 25
-    },
-    progressBar: {
-        backgroundColor: 'rgb(3, 154, 229)',
-        height: 3,
-        shadowColor: '#000',
-    }
-})
+  ProfilePhotoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Icon: {
+    backgroundColor: "#e4e7ff",
+    borderRadius: 100,
+    width: 35,
+    height: 34,
+    justifyContent: "center",
+  },
+  userInfo: {
+    marginLeft: 25,
+  },
+  progressBar: {
+    backgroundColor: "rgb(3, 154, 229)",
+    height: 3,
+    shadowColor: "#000",
+  },
+});
